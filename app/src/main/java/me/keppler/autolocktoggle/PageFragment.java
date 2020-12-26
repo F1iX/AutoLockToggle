53bc65db4611195d1bb5f67af60a9cdf6ec793b3
Felix Keppler <felix@keppler.me>
2020-12-27 00:14:16 +0100

    Setup guide working with pages

--- app/src/main/java/me/keppler/autolocktoggle/PageFragment.java
+++ app/src/main/java/me/keppler/autolocktoggle/PageFragment.java
@@ -5,13 +5,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class PageFragment extends Fragment {
-    private int i;
+    private int pos;
 
-    PageFragment(int i){
-        this.i = i;
+    PageFragment(int pos){
+        this.pos = pos;
     }
 
     @Override
@@ -19,8 +21,36 @@ public class PageFragment extends Fragment {
                              Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(
                 R.layout.fragment_screen_slide_page, container, false);
-        TextView textView = (TextView) rootView.findViewById(R.id.mytext);
-        textView.setText(Integer.toString(i));
+        TextView textView = (TextView) rootView.findViewById(R.id.desc_text);
+        ImageView imageView = (ImageView) rootView.findViewById(R.id.desc_img);
+        ImageView imageView2 = (ImageView) rootView.findViewById(R.id.desc_img2);
+        Button button = (Button) rootView.findViewById(R.id.desc_button);
+
+        switch (pos){
+            case 0:
+                imageView.setImageDrawable(getResources().getDrawable(R.drawable.desc0_adb_grant_permission, null));
+                textView.setText(getText(R.string.desc0));
+                button.setVisibility(View.GONE);
+                break;
+            case 1:
+                imageView.setImageDrawable(getResources().getDrawable(R.drawable.desc1_disable_power_instant_lock, null));
+                textView.setText(getText(R.string.desc1));
+                button.setVisibility(View.GONE);
+                break;
+            case 2:
+                imageView.setImageDrawable(getResources().getDrawable(R.drawable.desc2_1_select_modify_system_settings, null));
+                imageView2.setImageDrawable(getResources().getDrawable(R.drawable.desc2_2_enable_modify_system_settings, null));
+                textView.setText(getText(R.string.desc2));
+                button.setVisibility(View.GONE);
+                break;
+            default:
+                imageView.setImageDrawable(getResources().getDrawable(R.drawable.desc3_1_edit_drawer, null));
+                imageView2.setImageDrawable(getResources().getDrawable(R.drawable.desc3_2_add_tile, null));
+                textView.setText(getText(R.string.desc3));
+                button.setVisibility(View.GONE);
+                break;
+
+        }
         return rootView;
     }
 }
