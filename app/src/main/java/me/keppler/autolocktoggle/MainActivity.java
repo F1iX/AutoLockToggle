bbf27364d3ca46884fa9432f98fa5294227d57b1
Felix Keppler <felix@keppler.me>
2020-12-26 20:13:57 +0100

    ViewPager2 with dot indicator working

--- app/src/main/java/me/keppler/autolocktoggle/MainActivity.java
+++ app/src/main/java/me/keppler/autolocktoggle/MainActivity.java
@@ -10,12 +10,18 @@ import android.os.Bundle;
 import android.provider.Settings;
 import android.util.Log;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final int NUM_PAGES = 5;
 
     // The pager widget, which handles animation and allows swiping horizontally to access previous and next wizard steps
     private ViewPager2 viewPager;
+
+    // Tab layout for indicator dots
+    private TabLayout tabLayout;
 
     // The pager adapter, which provides the pages to the view pager widget
     private FragmentStateAdapter pagerAdapter;
@@ -30,6 +36,11 @@ public class MainActivity extends AppCompatActivity {
         viewPager = findViewById(R.id.pager);
         pagerAdapter = new ScreenSlidePagerAdapter(this);
         viewPager.setAdapter(pagerAdapter);
+
+        tabLayout = findViewById(R.id.tabs);
+        // Attach TabLayoutMediator automatically updating active tab dot indicator
+        new TabLayoutMediator(tabLayout, viewPager, (tab, position) -> {
+        }).attach();
     }
 
     /**
